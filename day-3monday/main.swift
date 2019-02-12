85cffd0a9f7b485755120660bc0cc9ee93d50e20
MacStudent <macstudent@126-iMac08.local>
2019-02-11 19:12:19 -0500

    simple interest

--- day-3monday/main.swift
+++ day-3monday/main.swift
@@ -42,4 +42,32 @@ func sub(_ a:Float,_ b:Float)
 
  sub(20,10)
 
+func greet(name:String)->String
+{
+    let s="welcome,\(name)"
+    return s;
+}
+
+print(greet(name: "samir"))
+
+func exchange(a:Int,b:Int)->(Int,Int)
+{
+    return (b,a)
+}
+
+var x=100
+var y=300
+print(x,y)
+(x,y)=exchange(a: x, b: y)
+print(x,y)
+
+// simple interest
+
+func si(amount:Float=100,rate:Float,year:Float=2) -> Float {
+return (amount*rate*year)/100
+}
+
+print(si(rate:2.0))
+
+
 
