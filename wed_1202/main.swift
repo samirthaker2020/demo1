2911832cd5b135dddb2cba802d0f32749aca30e6
MacStudent <macstudent@126-iMac15.local>
2019-02-13 18:08:12 -0500

    Initial Commit

---
+++ wed_1202/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wed_1202
+//
+//  Created by MacStudent on 2019-02-13.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
