b377cc9ea9f2cda33d60bd93cb97a5141b6e993d
MacStudent <macstudent@126-iMac15.local>
2019-02-13 18:36:19 -0500

    new project created with new class and how to  deal with null collision operator

--- wed_1202/main.swift
+++ wed_1202/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+
+class Person
+{
+    
+    var pid:Int=0
+    var fname:String!
+    var lname:String?
+    
+    func setdata()
+    {
+        lname="thaker"
+    }
+    func display()
+    {
+        print(pid)
+        print(fname)
+       // print(lname)
+       // let l = lname ?? "thaker"
+        if let f = fname, let l = lname
+        {
+        let s = f + " " + l
+        print(s)
+        }
+        
+    }
+}
+
+
+var p1=Person()
+p1.setdata()
+p1.display()
 
