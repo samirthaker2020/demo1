65883612f2118f89bd4a8b958e04601b007c3fd4
MacStudent <macstudent@126-iMac15.local>
2019-02-13 18:53:03 -0500

    constructor

--- wed_1202/main.swift
+++ wed_1202/main.swift
@@ -28,7 +28,8 @@ class Person
        // let l = lname ?? "thaker"
         if let f = fname, let l = lname
         {
-        let s = f + " " + l
+        let s = f
+            + " " + l
         print(s)
         }
         
@@ -36,7 +37,37 @@ class Person
 }
 
 
-var p1=Person()
-p1.setdata()
-p1.display()
+//var p1=Person()
+//p1.setdata()
+//p1.display()
 
+class Student
+{
+    var sid:Int
+    var fname:String
+    var lname:String
+    init()
+    {
+        sid=0
+        fname=String()
+        lname=String()
+    }
+    init(sid:Int,fname:String,lname:String){
+        self.sid=sid
+        self.fname=fname
+        self.lname=lname
+    }
+    
+    func disp()
+    {
+        print("Student ID:::\(self.sid)")
+        print("Student Name::\(self.fname)")
+        print("Student LastName::\(self.lname)")
+    }
+    
+}
+
+var s1=Student()
+s1.disp()
+let s2=Student(sid: 01, fname: "samir", lname: "thaker")
+s2.disp()
